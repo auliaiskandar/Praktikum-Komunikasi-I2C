4f488ae8420ec0565a10663d78871a2ff78b560a
auliaiskandar <iskandar.bin.rusliahmad@gmail.com>
2016-10-19 06:41:06 +0700

    Komunikasi I2C

---
+++ Praktikum 1/Master_1_String/Master_1_String.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+void loop(){
+  Wire.requestFrom(2, 5);
+  while(Wire.available()){
+//    unsigned char dataI2C[2];
+//    dataI2C [0] = Wire.read();
+//    dataI2C [1] = Wire.read();
+//    unsigned int dataADC = word(dataI2C[1], dataI2C[0]);
+    char c = Wire.read();
+    Serial.print (c);
+  }
+  delay(500);
+}
